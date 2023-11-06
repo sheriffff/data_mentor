1ef12684e8073e5760a96051abbfdcccb6368545
sheriff <manuel.lopez@frenetic.ai>
2023-11-06 18:41:38 +0100

    Update openai version to 1.0.1

--- gpt.py
+++ gpt.py
@@ -1,8 +1,8 @@
-import openai
 from typing import List
 from config import OPENAI_KEY
+from openai import OpenAI
 
-openai.api_key = OPENAI_KEY
+client = OpenAI(api_key=OPENAI_KEY)
 
 
 def ask_gpt(system_role: str, user_messages: List[str]):
@@ -11,7 +11,7 @@ def ask_gpt(system_role: str, user_messages: List[str]):
     for user_message in user_messages:
         messages.append({"role": "user", "content": user_message})
 
-    response_gpt = openai.ChatCompletion.create(
+    response_gpt = client.chat.completions.create(
         model="gpt-4",
         messages=messages
     )
