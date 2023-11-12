eab3f5df685aa051d371e3a831fc4d96417f4443
sheriff <manuel.lopez@frenetic.ai>
2023-11-12 18:22:47 +0100

    Upgrade app

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-import random
-
 import streamlit as st
 
-from config import langs, Tools, SPINNER_TEXT, SPINNER_TEXT_IMAGINE, random_concepts, MESSAGE_RANDOM_CONCEPT
+from config import Tools, SPINNER_TEXT, language_code_to_name, SPINNER_TEXT_EXERCISE, explanation_length_code_to_name
 from gpt import ask_gpt
 
 
@@ -19,14 +17,31 @@ def set_page_config():
     )
 
 
+def ask_for_response_language():
+    language_codes = list(language_code_to_name.keys())
+    n_langs = len(language_codes)
+    if n_langs > 1:
+        st.session_state.language_code = st.sidebar.radio("Lenguaje de respuesta", options=language_codes, index=0)
+    else:
+        st.session_state.language_code = list(language_code_to_name.keys())[0]
+
+
 def build_sidebar():
     st.sidebar.header("PyData Mentor 🧑‍🏫")
     # no selection by default in selectbox
-    use_case = st.sidebar.selectbox("Tool", (Tools.CONCEPT_ILLUMINATOR, Tools.PUZZLE_BUILDER, Tools.DATASET_FINDER, Tools.ABOUT),
-                                    key="use_case", index=0)
+    use_case = st.sidebar.selectbox(
+        "Herramienta", (
+            Tools.CONCEPT_ILLUMINATOR,
+            Tools.PUZZLE_BUILDER,
+            # Tools.DATASET_FINDER,
+            Tools.ABOUT
+        ),
+        key="use_case", index=0
+    )
 
-    st.session_state.language = st.sidebar.radio("Response Language", options=langs, index=0)
-    st.sidebar.markdown("done by [sheriff](https://linkedin.com/in/sheriff-data)")
+    ask_for_response_language()
+
+    st.sidebar.markdown("by [sheriff](https://linkedin.com/in/sheriff-data)")
 
     if use_case == Tools.CONCEPT_ILLUMINATOR:
         page_concept()
@@ -39,54 +54,58 @@ def build_sidebar():
 
 
 def page_index():
-    st.header("Welcome to PyData Mentor 🧑‍🏫️")
-    st.subheader("A series of tools to help you learn Python and Data Science")
+    st.header("Bienvenid@ a PyData Mentor 🧑‍🏫️")
+    st.subheader("Una serie de herramientas para ayudarte a aprender Python y Data Science")
     st.markdown(f"""
-    - {Tools.CONCEPT_ILLUMINATOR}: review a concept and ask for real world examples
+    - {Tools.CONCEPT_ILLUMINATOR}: explica un concepto y proporciona ejemplos del mundo real
     
-    - {Tools.PUZZLE_BUILDER}: generate an exercise to test your knowledge
+    - {Tools.PUZZLE_BUILDER}: genera un ejercicio para poner a prueba tus conocimientos
     
-    - {Tools.DATASET_FINDER}: find a dataset to practice your skills
+    
+    Creado por [sheriff](https://linkedin.com/in/sheriff-data)
     """)
 
+    # - {Tools.DATASET_FINDER}: find a dataset to practice your skills
 
-def build_concept_query(concept, explanation_type: str, explanation_length: str, is_with_use_case: bool):
-    query_llm = f"Explain the concept of {concept}"
 
-    if explanation_type == '5 year old':
-        query_llm += " as if I were five years old."
-    elif explanation_type == 'Basic':
-        query_llm += " in simple terms."
-    elif explanation_type == 'Intermediate':
-        query_llm += " at an intermediate level of complexity."
-    elif explanation_type == 'Expert':
-        query_llm += " with expert level detail."
+def build_concept_query(concept, explanation_type: str, explanation_length_code: str, is_with_use_case: bool):
+    query_llm = f"Explica el concepto de {concept}"
 
-    query_llm += f"The explanation length should be: {explanation_length}."
-    if explanation_length != 'Very short':
-        query_llm += " Please provide the explanation in several distinct lines."
+    if explanation_type == '5 años':
+        query_llm += " como si tuviera 5 años. "
+    elif explanation_type == 'Básico':
+        query_llm += " a un nivel básico. "
+    elif explanation_type == 'Intermedio':
+        query_llm += " a un nivel intermedio. "
+    elif explanation_type == 'Experto':
+        query_llm += " a un nivel experto. "
+
+    query_llm += f"La longitud de la explicación debe ser: {explanation_length_code_to_name[explanation_length_code]}. "
+
+    if explanation_length_code != 'XS':
+        query_llm += "Por favor, no hagas párrafos muy largos. "
 
     if is_with_use_case:
-        query_llm += " Include a real-world example of how this concept is applied."
-
-    query_llm += "I will write the explanation using markdown. So please use markdown syntax, especially for bold and italic."
-    query_llm += "In particular, start with a header3 as title."
+        query_llm += "Por favor, incluye un ejemplo del mundo real de cómo se aplica este concepto. "
 
     return query_llm
 
 
 def ask_for_concept_inputs():
-    random_concept = random.choice(random_concepts)
     concept = st.text_input(
-        'Ask about any Python/Data related concept',
+        '¿Qué concepto quieres entender/repasar?',
         # placeholder=random_concept.lower()
     )
 
     col1, col2 = st.columns(2)
-    explanation_type = col1.radio('Explanation level', options=['5 year old', 'Basic', 'Intermediate', 'Expert'],
+    explanation_type = col1.radio('Nivel de la explicación', options=['5 años', 'Básico', 'Intermedio', 'Experto'],
                                   index=1)
-    explanation_length = col2.radio('Explanation length', options=['Very short', 'Short', 'Medium', 'Long'], index=1)
-    is_with_use_case = st.checkbox('Add a real world example')
+    explanation_length = col2.radio('Longitud de la explicación', options=['XS', 'S', 'M', 'L'], index=1)
+
+    if explanation_length in ['M', 'L']:
+        is_with_use_case = st.checkbox('Incluye un ejemplo del mundo real')
+    else:
+        is_with_use_case = False
 
     return concept, explanation_type, explanation_length, is_with_use_case
 
@@ -96,68 +115,63 @@ def page_concept():
 
     concept, explanation_type, explanation_length, is_with_use_case = ask_for_concept_inputs()
 
-    if st.button('GO'):
+    if st.button('GO!'):
         query_llm = build_concept_query(concept, explanation_type, explanation_length, is_with_use_case)
 
         with st.spinner(SPINNER_TEXT):
             explanation = ask_gpt(
                 user_message=query_llm,
-                language_response=st.session_state.language
+                language_response=language_code_to_name[st.session_state.language_code]
             )
 
         st.markdown(explanation)
 
-    if st.button('GO RANDOM'):
-        with st.spinner(SPINNER_TEXT_IMAGINE):
-            explanation = ask_gpt(
-                user_message=MESSAGE_RANDOM_CONCEPT,
-                language_response=st.session_state.language
-            )
-
-        st.markdown(explanation)
+    # if st.button('SORPRÉNDEME!'):
+    #     with st.spinner(SPINNER_TEXT_IMAGINE):
+    #         explanation = ask_gpt(
+    #             user_message=MESSAGE_RANDOM_CONCEPT,
+    #             language_response=st.session_state.language_code,
+    #         )
+    #
+    #     st.markdown(explanation)
 
 
 def ask_for_exercise_inputs():
-    topic = st.text_input('Ask for an exercise on the topic you want')
+    topic = st.text_input('¿Sobre qué tema / concepto quieres el ejercicio?')
 
     col1, col2 = st.columns(2)
-    difficulty_level = col1.radio('Difficulty level', options=['Basic', 'Intermediate', 'Expert'], index=0)
+    difficulty_level = col1.radio('Nivel', options=['Básico', 'Intermediato', 'Experto'], index=0)
 
-    exercise_type = col2.radio('Exercise type', options=['Multiple-choice', 'Fill-in-the-blank', 'Code-writing', 'Debugging'], index=0)
-
-    return topic, difficulty_level, exercise_type
+    return topic, difficulty_level
 
 
-def build_puzzle_query(topic, difficulty_level, exercise_type):
-    query_llm = f"Build an exercise of difficulty level: {difficulty_level}. "
-    query_llm += f"Exercise must be exercise about topic: {topic}. "
-    query_llm += f"Exercise must be of type: {exercise_type}. "
-    query_llm += "Do not write intros or explanations. Just the exercise. "
-    query_llm += "You will add an extra line at the end with the exact syntax: '--Solution--' followed by the solution with an explanation."
+def build_puzzle_query(topic, difficulty_level):
+    query_llm = f"Crea un ejercicio de código de dificultad {difficulty_level} sobre el tema '{topic}'. "
+    query_llm += "Por favor, no escribas nada más que el ejercicio. "
+    query_llm += "Al final, añadirás una línea extra con la sintaxis exacta: '--Solución--' seguida de la solución con una explicación. "
 
     return query_llm
 
 
 def page_exercise():
     st.header(Tools.PUZZLE_BUILDER)
-    topic, difficulty_level, exercise_type = ask_for_exercise_inputs()
+    topic, difficulty_level = ask_for_exercise_inputs()
 
-    query_llm = build_puzzle_query(topic, difficulty_level, exercise_type)
+    query_llm = build_puzzle_query(topic, difficulty_level)
 
-    if st.button('GO'):
-        with st.spinner(SPINNER_TEXT):
+    if st.button('GO!'):
+        with st.spinner(SPINNER_TEXT_EXERCISE):
             response = ask_gpt(
                 user_message=query_llm,
-                language_response=st.session_state.language
+                language_response=language_code_to_name[st.session_state.language_code],
             )
 
-            puzzle, solution = response.split("--Solution--")
+            puzzle, st.session_state.solution = response.split("--Solución--")
 
-        st.subheader("Puzzle")
         st.markdown(puzzle)
 
-        with st.expander("Show solution"):
-            st.markdown(solution)
+        with st.expander("Ver solución"):
+            st.markdown(st.session_state.solution)
 
 
 def page_dataset():
